db12004896a82102f798dead2c699193ce6fc5e2
sfxdevops <sfxdevops@seamfix.com>
2023-11-06 15:19:57 +0100

    btidevops: added plaform-tenant-ms project

---
+++ Platform-tenant-ms/ecr.tf
@@ -0,0 +1,15 @@
+resource "aws_ecr_repository" "platform-tenant-ecr" {
+  name                 = "${var.project}/${var.project_component}"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    createdby   = var.createdby
+    project     = var.project
+    Name        = var.project_component
+    environment = var.environment
+  }
+}
