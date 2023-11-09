ab2f972292bbda4a328bc340c8e0fa6a7a01eda1
sfxdevops <sfxdevops@seamfix.com>
2023-11-09 18:41:16 +0100

    btidevops: aded new services

---
+++ mfb-subscriber-update-job-ms/ecr.tf
@@ -0,0 +1,15 @@
+resource "aws_ecr_repository" "mfb-subscriber-update-job-ms-ecr" {
+  name                 = "${var.project}/${var.project_component}"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    createdby   = var.createdby
+    project     = var.project
+    Name        = var.project_component
+    environment = var.environment
+  }
+}
---
+++ mfb-subscriber-update-job-ms/vars.tf
@@ -0,0 +1,41 @@
+variable "region" {
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+variable "aws_profile" {
+  default = "ddennis"
+}
+
+# terraform {
+#   backend "s3" {}
+# }
+
+variable "manager" {
+  type = list(string)
+  default = [
+    "terraform",
+    "cloudformation",
+    "console"
+  ]
+}
+
+variable "createdby" {
+  default = "echiejina"
+}
+
+variable "project" {
+  default = "mfb"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "project_component" {
+  default = "subscriber-update-job-ms"
+}
+
+variable "tech" {
+  default = "node"
+}
