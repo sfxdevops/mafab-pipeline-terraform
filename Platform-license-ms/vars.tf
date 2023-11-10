13e9584856e9c382f1c2b81b3b7ecb4354eadd3a
sfxdevops <sfxdevops@seamfix.com>
2023-11-10 17:17:16 +0100

    btidevops: added plaform-license-ms project

---
+++ Platform-license-ms/vars.tf
@@ -0,0 +1,40 @@
+variable "region" {
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+variable "aws_profile" {
+  default = "ddennis"
+}
+
+variable "manager" {
+  type = list(string)
+  default = [
+    "terraform",
+    "cloudformation",
+    "console"
+  ]
+}
+
+variable "createdby" {
+  default = "ddennis"
+}
+# variable "createdby" {
+#   default = "echiejina"
+# }
+
+variable "project" {
+  default = "sfx-platform"
+}
+
+variable "environment" {
+  default = "staging"
+}
+
+variable "project_component" {
+  default = "license-ms"
+}
+
+variable "tech" {
+  default = "node"
+}
