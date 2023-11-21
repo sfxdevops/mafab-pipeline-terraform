3041fd80ddde064fa33d00c1383afc2f2b661d66
sfxdevops <sfxdevops@seamfix.com>
2023-11-21 17:17:38 +0100

    btidevops: added plaform-issue-mgmt-ms project

---
+++ Platform-issue-mgmt-ms/codebuild_vars.tf
@@ -0,0 +1,23 @@
+variable "codebuild_role" {
+  default = "scripts/codebuild_role.json"
+}
+
+variable "codebuild_role_policy" {
+  default = "scripts/codebuild_role_policy.json"
+}
+
+variable "buildspec_java" {
+  default = "scripts/buildspec-java.yml"
+}
+
+variable "buildspec_node" {
+  default = "scripts/buildspec-node.yml"
+}
+
+variable "code_repo" {
+  default = "https://github.com/seamfix/platform-issue-mgmt-ms.git"
+}
+
+variable "repo_branch" {
+  default = "snapshot-dev"
+}
