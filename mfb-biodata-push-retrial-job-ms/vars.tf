7143227d4098c62ae94ef2be9d55a6d54e69c916
sfxdevops <sfxdevops@seamfix.com>
2023-11-26 17:36:36 +0100

    btidevops: added biodata-push-retrial-job project

---
+++ mfb-biodata-push-retrial-job-ms/vars.tf
@@ -0,0 +1,40 @@
+variable "region" {
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+variable "aws_profile" {
+  default = "ddennis"
+}
+# terraform {
+#   backend "s3" {}
+# }
+
+variable "manager" {
+  type = list(string)
+  default = [
+    "terraform",
+    "cloudformation",
+    "console"
+  ]
+}
+
+variable "createdby" {
+  default = "echiejina"
+}
+
+variable "project" {
+  default = "mfb"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "project_component" {
+  default = "biodata-push-retrial-job-ms"
+}
+
+variable "tech" {
+  default = "node"
+}
